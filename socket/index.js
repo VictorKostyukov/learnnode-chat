12a93ff79b7af688fad4384edcdb28d9f047c85d
Victor Kostyukov <vikost@outlook.com>
2014-12-12 12:35:39 +0800

    Added chat with socket.io
    Learn sockets.io added simple anonymous chat with reconnection retry.

---
+++ socket/index.js
@@ -0,0 +1,14 @@
+var log = require('libs/log')(module);
+
+module.exports = function(server) {
+    var io = require('socket.io').listen(server);
+    io.set('origins', 'localhost:*');
+    io.set('logger', log);
+
+    io.on('connection', function (socket) {
+        socket.on('message', function (text, callback) {
+            socket.broadcast.emit('message', text);
+            callback && callback();
+        });
+    });
+};
